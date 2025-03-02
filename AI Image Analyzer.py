6f80ebe2b6fd1c4493087ee69ec5facec5bb73da
George Telles <george_1@hotmail.com.br>
2025-03-02 18:44:06 -0300

    Add files via upload

---
+++ AI Image Analyzer.py
@@ -0,0 +1,38 @@
+from groq import Groq
+from dotenv import load_dotenv, find_dotenv
+import base64
+
+# Load environment variables
+_ = load_dotenv(find_dotenv())
+
+# Function to encode the image
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+# Path to your image
+image_path = "321.jpg"
+
+# Getting the base64 string
+base64_image = encode_image(image_path)
+
+client = Groq()
+completion = client.chat.completions.create(
+    model="llama-3.2-11b-vision-preview",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "What is in this image?"},
+                {"type": "image_url", "image_url": {"url": f"data:image/png;base64,{base64_image}"}}
+            ]
+        }
+    ],
+    temperature=1,
+    max_tokens=1024,
+    top_p=1,
+    stream=False,
+    stop=None,
+)
+
+print(completion.choices[0].message)
